0cc21c62ee5ae2d7ade3f497838576f1f5471544
TejaswiVallabhapurapu <tejaswivallabhapurapu@gmail.com>
2026-09-26 05:27:59 +0530

    Safely guard icon rendering in Notifications

--- client/src/pages/Notifications.jsx
+++ client/src/pages/Notifications.jsx
@@ -317,7 +317,7 @@ const Notifications = () => {
                   <div
                     className={`w-10 h-10 rounded-2xl flex items-center justify-center shrink-0 border ${config.color} shadow-2xs`}
                   >
-                    <Icon className="w-5 h-5" />
+                    {Icon ? <Icon className="w-5 h-5" /> : <span className="text-xs font-bold text-indigo-400">●</span>}
                   </div>
 
                   {/* Message & Meta */}
